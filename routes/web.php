d95e279026d9470fef8bdf0d8ec0e8be663d8f1f
Rohit Italiya <rohititaliaya@gmail.com>
2024-07-11 12:01:54 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
-Route::sesWebhooks('/webhooks/ses');
+// Route::sesWebhooks('/webhooks/ses');
+
+Route::post('/webhooks/ses', function(Request $request) {
+    $json = $request->all();
+    Log::info($json);
+});
