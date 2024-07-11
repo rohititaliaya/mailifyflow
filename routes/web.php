c7675f079e7efa70517793574727540be93261fe
Rohit Italiya <rohititaliaya@gmail.com>
2024-07-11 12:33:11 +0530

    routes repair

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
-// Route::sesWebhooks('/webhooks/ses');
+Route::sesWebhooks('/webhooks/ses');
 
-Route::post('/webhooks/ses', function(Request $request) {
+Route::post('/', function(Request $request) {
     $json = $request->all();
     Log::info($json);
 });
